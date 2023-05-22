450438419a69e558dcc78daa87c1c7163a74b48f
marleyschneiderr <marley33@uw.edu>
2023-05-21 20:26:17 -0700

    Adding navbar styling for mobile responsive

--- react-to-marley/src/components/Navbar.jsx
+++ react-to-marley/src/components/Navbar.jsx
@@ -1,12 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 import Logo from "../assets/MS.png";
 
 const Navbar = () => {
-
-  const [navbar, setNavbar] = useState(false)
-  const doClick = () => setNavbar(!navbar)
+  const [navbar, setNavbar] = useState(false);
+  const doClick = () => setNavbar(!navbar);
 
   return (
     <div className="fixed w-full h-[85px] flex justify-between items-center px-4 bg-[#f25c54] text-white">
@@ -23,12 +22,18 @@ const Navbar = () => {
       </ul>
 
       {/* Navbar hamburger */}
-      <div className="md:hidden z-10">
+      <div onClick={doClick} className="md:hidden z-10">
         <FaBars />
       </div>
 
       {/* Responsive for mobile menu */}
-      <ul className="hidden absolute top-0 left-0 w-full h-screen bg-[#f25c54] flex flex-col justify-center items-center">
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#f25c54] flex flex-col justify-center items-center"
+        }
+      >
         <li className="py-5 text-4xl">Home</li>
         <li className="py-5 text-4xl">Portfolio</li>
         <li className="py-5 text-4xl">Contact</li>
