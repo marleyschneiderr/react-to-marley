3be82af2d9821ae5379458c5e4c22f3f8c29e7e6
marleyschneiderr <marley33@uw.edu>
2023-05-21 19:36:09 -0700

    Adding new font, logo, and navbar functionality

--- react-to-marley/src/components/Navbar.jsx
+++ react-to-marley/src/components/Navbar.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import React, {useState} from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
-import Logo from '../assets/MS.png'
+import Logo from "../assets/MS.png";
 
 const Navbar = () => {
+
+  const [navbar, setNavbar] = useState(false)
+  const doClick = () => setNavbar(!navbar)
+
   return (
-    <div className='fixed w-full h-[85px] flex justify-between items-center px-4 bg-[#2D00F7] text-white'>
+    <div className="fixed w-full h-[85px] flex justify-between items-center px-4 bg-[#f25c54] text-white">
+      <div>
+        <img scr={Logo} alt="Logo Image" style={{ width: "50px" }} />
+      </div>
 
-        <div>
-            <img scr={Logo} alt="Logo Image" style={{width: '50px'}} />
-        </div>
-        <div>
-            <ul>
-                <li>Home</li>
-                <li>Portfolio</li>
-                <li>Contact</li>
-                <li>Resume</li>
-            </ul>
-        </div>
+      {/* Menu */}
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+        <li>Resume</li>
+      </ul>
 
+      {/* Navbar hamburger */}
+      <div className="md:hidden z-10">
+        <FaBars />
+      </div>
+
+      {/* Responsive for mobile menu */}
+      <ul className="hidden absolute top-0 left-0 w-full h-screen bg-[#f25c54] flex flex-col justify-center items-center">
+        <li className="py-5 text-4xl">Home</li>
+        <li className="py-5 text-4xl">Portfolio</li>
+        <li className="py-5 text-4xl">Contact</li>
+        <li className="py-5 text-4xl">Resume</li>
+      </ul>
+
+      {/* Social media icons */}
+      <div className="hidden"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
