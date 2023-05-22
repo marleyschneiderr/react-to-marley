28e9027ca5264f18f113469a92175bbf9ca392d3
marleyschneiderr <marley33@uw.edu>
2023-05-22 13:23:51 -0700

    Adding about me seciton

--- react-to-marley/src/components/Aboutme.jsx
+++ react-to-marley/src/components/Aboutme.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Aboutme = () => {
   return (
-    <div>Aboutme</div>
+    <div name="aboutme" className='w-full h-screen bg-[] text-white'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+
+        </div>
+        
+
+    </div>
   )
 }
 
