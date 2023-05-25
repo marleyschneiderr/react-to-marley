95c3d178f6f73b4efa40f381589473b66bf14b45
marleyschneiderr <marley33@uw.edu>
2023-05-24 19:45:23 -0700

    Adding navbar linkedin link

--- react-to-marley/src/components/Navbar.jsx
+++ react-to-marley/src/components/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
           <li className="w-[165p] h-[65px] flex justify-between items-center ml-[-100px] hover:ml-[10px] duration-300 bg-[#db00b6]">
             <a
               className="flex justify-between items-center w-full text-white-300"
-              href="/"
+              href="https://github.com/marleyschneiderr"
             >
               Github <FaGithub size={40} />
             </a>
