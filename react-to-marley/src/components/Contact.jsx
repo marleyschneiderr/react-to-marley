7d4d8b4abaf8cb78df43ee1d2d3fb73ca392131e
marleyschneiderr <marley33@uw.edu>
2023-05-28 16:30:01 -0700

    Updating contact

--- react-to-marley/src/components/Contact.jsx
+++ react-to-marley/src/components/Contact.jsx
@@ -6,18 +6,39 @@ const Contact = () => {
       name="contact"
       className="w-full h-screen bg-[#2d00f7] flex justify-center items-center p-4"
     >
-      <form method="POST" id="contactme" action="https://getform.io/f/06216a09-0dc6-43dd-be70-5a3164138d74" className="flex flex-col max-w-[700px] w-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-[#E500A4] text-white">
-            Contact Me
-          </p>
-          <p className="text-white py-5">Fill out the form below to contact me - marleysue@gmail.com</p>
-        </div>
-        <input className="bg-white p-2" type="text" placeholder="Your Name" name="name" />
-        <input className="my-4 p-2 bg-white" type="email" placeholder="example.email@example.com" name="email" />
-        <textarea className="bg-white p-2" name="message" rows="10" placeholder="Your Message Here"></textarea>
-        <button className="text-white border-2 hover:bg-[#E500A4] hover:border-[#E500A4] px-4 py-3 my-8 mx-auto flex items-center">Let's Talk</button>
-      </form>
+      <form method="POST" id="contactme" action="https://getform.io/f/06216a09-0dc6-43dd-be70-5a3164138d74" class="flex flex-col max-w-[700px] w-full">
+  <div class="pb-8">
+    <p class="text-4xl font-bold inline border-b-4 border-[#E500A4] text-white">
+      Contact Me
+    </p>
+    <p class="text-white py-5">Fill out the form below to contact me - marleysue@gmail.com</p>
+  </div>
+  <input class="bg-white p-2" type="text" placeholder="Your Name" name="name" />
+  <input class="my-4 p-2 bg-white" type="email" placeholder="example.email@example.com" name="email" />
+  <textarea class="bg-white p-2" name="message" rows="10" placeholder="Your Message Here"></textarea>
+  <button class="text-white border-2 hover:bg-[#E500A4] hover:border-[#E500A4] px-4 py-3 my-8 mx-auto flex items-center">Let's Talk</button>
+</form>
+
+<script>
+  const form = document.getElementById('contactme');
+  const inputs = form.querySelectorAll('input, textarea');
+
+  inputs.forEach((input) => {
+    input.addEventListener('blur', () => {
+      if (input.value.trim() === '') {
+        showNotification(input);
+      }
+    });
+  });
+
+  function showNotification(input) {
+    const notification = document.createElement('div');
+    notification.classList.add('text-red-500', 'text-sm', 'my-2');
+    notification.textContent = `${input.placeholder} is required`;
+    input.parentNode.insertBefore(notification, input.nextSibling);
+  }
+</script>
+
     </div>
   );
 };
