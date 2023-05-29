73e16deaef965f78c511665d7c587c16f733ad9a
marleyschneiderr <marley33@uw.edu>
2023-05-28 17:06:54 -0700

    Adding error for contact pages

--- react-to-marley/src/components/Contact.jsx
+++ react-to-marley/src/components/Contact.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  const handleBlur = (e) => {
+    const { name, value } = e.target;
+    if (value === "") {
+      setError(`Please enter your ${name}`);
+    } else {
+      setError("");
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name === "") {
+      setError("Please enter your name");
+    } else if (email === "") {
+      setError("Please enter your email");
+    } else if (message === "") {
+      setError("Please enter your message");
+    } else {
+      // Perform form submission
+      document.getElementById("contactme").submit();
+    }
+  };
+
   return (
     <div
       name="contact"
@@ -10,40 +38,66 @@ const Contact = () => {
         method="POST"
         id="contactme"
         action="https://getform.io/f/06216a09-0dc6-43dd-be70-5a3164138d74"
-        class="flex flex-col max-w-[700px] w-full"
+        className="flex flex-col max-w-[700px] w-full"
+        onSubmit={handleSubmit}
       >
-        <div class="pb-8">
-          <p class="text-4xl font-bold inline border-b-4 border-[#E500A4] text-white">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-[#E500A4] text-white">
             Contact Me
           </p>
-          <p class="text-white py-5">
+          <p className="text-white py-5">
             Fill out the form below to contact me - marleysue@gmail.com
           </p>
         </div>
         <input
-          class="bg-white p-2"
+          className="bg-white p-2"
           type="text"
           placeholder="Your Name"
           name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onBlur={handleBlur}
         />
         <input
-          class="my-4 p-2 bg-white"
+          className="my-4 p-2 bg-white"
           type="email"
           placeholder="example.email@example.com"
           name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          onBlur={handleBlur}
         />
         <textarea
-          class="bg-white p-2"
+          className="bg-white p-2"
           name="message"
           rows="10"
           placeholder="Your Message Here"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onBlur={handleBlur}
         ></textarea>
-        <button class="text-white border-2 hover:bg-[#E500A4] hover:border-[#E500A4] px-4 py-3 my-8 mx-auto flex items-center">
+        {error && (
+          <div className="notification">
+            <p className="text-red-500">{error}</p>
+          </div>
+        )}
+        <button
+          className="text-white border-2 hover:bg-[#E500A4] hover:border-[#E500A4] px-4 py-3 my-8 mx-auto flex items-center"
+          type="submit"
+        >
           Let's Talk
         </button>
       </form>
+      {error && (
+        <div
+          className="fixed bottom-0 left-0 w-full bg-red-500 text-white py-2 text-center"
+        >
+          {error}
+        </div>
+      )}
     </div>
   );
 };
 
 export default Contact;
+
