49ab04455d3a7caa666ddd04483a614d012d449a
marleyschneiderr <marley33@uw.edu>
2023-05-28 17:19:42 -0700

    Adding more icons in the bottom as requested

--- react-to-marley/src/App.js
+++ react-to-marley/src/App.js
@@ -1,5 +1,6 @@
 import Aboutme from "./components/Aboutme";
 import Contact from "./components/Contact";
+import { Footer } from "./components/Footer";
 import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
 import Programs from "./components/Programs";
@@ -15,6 +16,7 @@ function App() {
       <Programs />
       <Projects />
       <Contact />
+      <Footer/>
     </div>
   );
 }
---
+++ react-to-marley/src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FaBars, FaTimes, FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
+
+export const Footer = () => {
+  return (
+    <footer className="fixed bottom-4 right-4 flex justify-end items-center">
+      <div className="flex">
+        <a
+          className="w-[165px] h-[65px] text-white flex justify-between items-center ml-[-100px] hover:ml-[10px] duration-300 bg-[#2d00f7]"
+          href="https://github.com/marleyschneiderr"
+        >
+            <FaGithub size={40} />
+        </a>
+        <a
+          className="w-[165px] h-[65px] text-white flex justify-between items-center ml-[-100px] hover:ml-[10px] duration-300 bg-[#2d00f7]"
+          href="https://www.linkedin.com/in/marley-schneider/"
+        >
+          <FaLinkedin size={40} />
+        </a>
+        <a
+          className="w-[165px] h-[65px] text-white flex justify-between items-center ml-[-100px] hover:ml-[10px] duration-300 bg-[#2d00f7]"
+          href="https://www.instagram.com/marleyschneiderr/"
+        >
+          <FaInstagram size={40} />
+        </a>
+      </div>
+    </footer>
+  );
+};
